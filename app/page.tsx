5949b59153d2ab15f154eac0bad2f465f2dcdd7f
ZaffService <seckmoustapha238@gmail.com>
2026-02-27 11:35:59 +0000

    commenter pour l'instant la section partenaire

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ import HeroBanner from "@/components/hero-banner";
 import WhyChooseBibocom from "@/components/why-choose-bibocom";
 import HowItWorks from "@/components/how-it-works";
 import { Testimonials } from "@/components/testimonials";
-import TrustCarousel from "@/components/TrustCarousel";
+// import TrustCarousel from "@/components/TrustCarousel";
 import { CourseCard } from "@/components/course-card";
 import MaintenancePage from "@/components/MaintenancePage";
 import EmptyCoursesState from "@/components/EmptyCoursesState";
@@ -447,7 +447,7 @@ const Index = () => {
 
       {/* Section Témoignages -仅 pour les utilisateurs non connectés */}
       <Testimonials />
-      <TrustCarousel />
+      {/* <TrustCarousel /> */}
 
       <a
         href="https://wa.me/221788784040"
